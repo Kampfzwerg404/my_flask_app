ba730837cc0c3e15a323893e485c19c6faf0a068
Kampfzwerg404 <bublikovkolacik@gmail.com>
2024-12-10 14:19:25 +0300

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
-   from flask import Flask, jsonify
+from flask import Flask, jsonify
 
-   app = Flask(__name__)
+app = Flask(__name__)
 
-   @app.route('/')
-   def home():
-       return jsonify({"message": "Welcome to my Flask API!"})
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to my Flask API!"})
 
-   @app.route('/api/data', methods=['GET'])
-   def get_data():
-       return jsonify({"data": "This is some sample data."})
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify({"data": "This is some sample data."})
 
-   if __name__ == '__main__':
-       app.run(host='0.0.0.0', port=5000)
-   
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
